06822f6120c11dbed1a1f4300671b517ee9699a2
Mal Nair <malna1r@users.noreply.github.com>
2015-05-24 02:10:24 -0700

    Update boredom.php

--- boredom.php
+++ boredom.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="chrome=1">
-    <title>Bored Today</title>
-
-    <link rel="stylesheet" href="boredtoday.css">
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <script src="boredtoday.js" type="text/javascript"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  
-  </head>
-  <body>
-    <h1 align="center">Bored Today? Here's Somewhere For You To Go</h1>
-
 <?php
 /**
  * Yelp API v2.0 code sample.
@@ -37,6 +21,7 @@ require_once('OAuth.php');
 // Set your OAuth credentials here  
 // These credentials can be obtained from the 'Manage API Access' page in the
 // developers documentation (http://www.yelp.com/developers)
+$radius = $_GET["distance"];
 $CONSUMER_KEY = '4TrZPvNHDTQrgfPF3yQAXQ';
 $CONSUMER_SECRET = 'gEcc0LEUsneDbhZQGNIWeldbqqU';
 $TOKEN = 'Qq0sE3x5OP3Pe-znqxSlXX4gbJi8dPQ9';
@@ -48,8 +33,7 @@ $DEFAULT_LOCATION = substr($country, strpos($country, "City")+6, strpos($country
 $SEARCH_LIMIT = 20;
 $SEARCH_PATH = '/v2/search/';
 $BUSINESS_PATH = '/v2/business/';
-$url = "";
-$img = "";
+$second = true;
 /** 
  * Makes a request to the Yelp API and returns the response
  * 
@@ -100,6 +84,10 @@ function search($term, $location) {
     $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
     $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
     $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
+    $url_params['sort'] = '1';  
+    if (isset($_GET['distance'])) {
+        $url_params['radius_filter'] = $GLOBALS['radius'];
+    }
     $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
     
     return request($GLOBALS['API_HOST'], $search_path);
@@ -131,7 +119,6 @@ function query_api($term, $location) {
         
     }
     $num =mt_rand(0, count($ids)-1);
-    print($num);
     $responsee = get_business_name($ids[$num]);
     $json = json_decode($responsee);
     $name = $json->name;
@@ -140,35 +127,36 @@ function query_api($term, $location) {
     $img = $json->image_url;
     $location = json_decode(json_encode($location, true));
     $add = $location->display_address;
-    
-    //Deals with the awkward "the" grammar issue
-    $locationwords = explode(" ", name);
-    if(strcmp("the", strtolower($locationwords[0])) !== 0) {
-      //concatenate "the" to the end of the preface phrase
-    } 
-
-    foreach ($add as $ad) {
-        if(is_null($address)) {
-            $address = $ad;
-        }else {
-        $address = $address."'%2C%'".$ad;
-        }
-    }
-    $address = str_replace(" ", '%20', $address);
-    
-    $url = "https://www.google.com/maps/embed/v1/place?q=".$address."&key=AIzaSyDunxkbh0Nr7LiIhQ7aDdxGH-EZWDzLaS8";
-    ?>
-
-<iframe width="600" height="450" frameborder="0" style="border:0" src=<?=$url?>></iframe>
-
-        <div id="business">
-            <h2><?= $name ?></h2>
-            <img src=<?= $img ?>>
-        </div>
-<?php
+    $city = $location->city;
+    $statecode=$location->state_code;
+    $urlPg = $json->url;
+    $address = $name.",".$city.",".$statecode;
+    $address = urlencode($address);
+    $url = "https://www.google.com/maps/embed/v1/place?q=".$address."&key=AIzaSyDunxkbh0Nr7LiIhQ7aDdxGH-EZWDzLaS8&maptype=satellite&zoom=17";   
+    return array($name, $img, $url, $rating, $urlPg);
 }
+/**
+ * User input is handled here 
+ */
+$longopts  = array(
+    "term::",
+    "location::",
+);
 
-function rating() {
+$prefacetext = array("Have you considered: ",
+                     "Are you interested in: ",
+                     "Want to check out: ",
+                     "Try: ",
+                     "You should hit up: ",
+                     "Are you bored enough to go to: ",
+                     "What about: ",
+                     "Have you thought of: ",
+                     "Ever been to: ",
+                     "Consider: "
+);
+
+
+function rating($rating) {
 if($rating == 5) {
     return "People on Yelp think it's incredible";
 } elseif($rating >= 4.5 && $rating < 5) {
@@ -193,18 +181,51 @@ if($rating == 5) {
     return "Don't go here. I didn't even think it was possible to for something to be rated this low on Yelp";
 }
 }
-/**
- * User input is handled here 
- */
-$longopts  = array(
-    "term::",
-    "location::",
-);
+
+
+$num = mt_rand(0, count($prefacetext) - 1);
     
 $options = getopt("", $longopts);
 $term = $options['term'] ?: '';
 $location = $options['location'] ?: '';
-query_api($term, $location);
+list($name, $img, $url, $rating, $urlPg) = query_api($term, $location);
+$rate = rating($rating);
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Coday15team2 by malna1r</title>
+
+    <link rel="stylesheet" href="bored.css">
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <link href="bored.css" type="text/css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  
+  </head>
+  <body>
+    <h1>Bored Today? </h1>
+       
+        <h2 id="preface"><?= $prefacetext[$num]?></h2>
+        <div id="name"><p><a href="<?=$urlPg?>"><?= $name ?></a></p></div>
+        <div id="wit"><?= $rate ?></div>
+        <div id="info"></div>
+        <div class="bar"></div>
+        <div id="map-canvas"><iframe width="600" height="450" frameborder="0" style="border:0" color="red" src=<?=$url?>></iframe></div>
+        <div class="bar"></div>
+
+        <div id="option">
+            <h3> How are you planning on getting where you're going? </h3> 
+             <form name="distance" action="test.php" method="get">
+                <select id="distance" name="distance">
+                    <option value="1609" >Walking</option>
+                    <option value="8046">Biking</option>
+                    <option value="24140">Driving</option>
+                </select>
+                <input type="submit" value="Try Another" />
+            </form>
+        </div>
   </body>
 </html>
